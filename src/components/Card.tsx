d48e7facab85724131a671da54d474cad1328310
Daniil Kutyavin <daniil.kutyavin94@gmail.com>
2020-07-20 18:27:30 +0300

    Allow follow the url

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,7 +3,9 @@ import { Item } from "../../interfaces/api";
 export function Card(item: Item) {
   return (
     <div className="py-6 pl-5 rounded-lg shadow-sm">
-      <h1>{item.title}</h1>
+      <a target="_blank" rel="noreferrer" href={item.url}>
+        <h1>{item.title}</h1>
+      </a>
     </div>
   );
 }
