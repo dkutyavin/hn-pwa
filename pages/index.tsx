54111cf599034aae034b65943596703e2b0f100d
Daniil Kutyavin <daniil.kutyavin94@gmail.com>
2020-07-20 13:51:18 +0300

    add tailwindcss

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="max-w-sm min-h-100 m-auto flex p-6 bg-white rounded-lg shadow-xl">Hello</div>
+  )
+}
