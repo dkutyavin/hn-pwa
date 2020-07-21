489878428f349207145abe663dd9182c6dc541b3
Daniil Kutyavin <daniil.kutyavin94@gmail.com>
2020-07-21 13:33:44 +0300

    Show active path

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export function Header() {
   return (
-    <header className="bg-orange-600 py-2">
-      <nav className="justify-evenly flex min-w-full">
+    <header className="bg-orange-600">
+      <nav className="justify-evenly flex w-full">
         <NavItem url="/">Top</NavItem>
         <NavItem url="/best">Best</NavItem>
         <NavItem url="/new">New</NavItem>
@@ -18,9 +19,20 @@ interface NavItemProps {
 }
 
 function NavItem({ url, children }: NavItemProps) {
+  const isActive = useIsActive(url);
+  let classes = "text-white flex-1 text-center h-full py-2";
+  if (isActive) {
+    classes += " bg-orange-700";
+  }
+
   return (
     <Link href={url}>
-      <a className="text-white">{children}</a>
+      <a className={classes}>{children}</a>
     </Link>
   );
+}
+
+function useIsActive(path: string) {
+  const router = useRouter();
+  return router.pathname === path;
 }
