c23a8d080c218a2032a2498a94898ae339b6e9f0
Daniil Kutyavin <daniil.kutyavin94@gmail.com>
2020-07-21 12:44:20 +0300

    Add List component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,10 @@
-import { Item } from "../interfaces/api";
-import { Card } from "../src/components/Card";
-import { useRequest } from "../src/utils/useRequest";
 import Layout from "../src/components/Layout";
+import List from "../src/components/List";
 
 export default function Home() {
-  const { data, status } = useRequest<Item[]>("/api/top");
-
-  if (status === "error") return <div>failed to load</div>;
-  if (status === "fetching") return <div>loading...</div>;
-
   return (
     <Layout>
-      {data.map(item => (
-        <Card key={item.id} {...item} />
-      ))}
+      <List type="top" />
     </Layout>
   );
 }
--- pages/new.tsx
+++ pages/new.tsx
@@ -1,19 +1,10 @@
-import { Item } from "../interfaces/api";
-import { Card } from "../src/components/Card";
-import { useRequest } from "../src/utils/useRequest";
 import Layout from "../src/components/Layout";
+import List from "../src/components/List";
 
 export default function New() {
-  const { data, status } = useRequest<Item[]>("/api/new");
-
-  if (status === "error") return <div>failed to load</div>;
-  if (status === "fetching") return <div>loading...</div>;
-
   return (
     <Layout>
-      {data.map(item => (
-        <Card key={item.id} {...item} />
-      ))}
+      <List type="new" />
     </Layout>
   );
 }
