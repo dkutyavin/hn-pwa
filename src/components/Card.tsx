235f490eb87aa7a4932b7dfcef9fa4f6be53e9be
Daniil Kutyavin <daniil.kutyavin94@gmail.com>
2020-07-21 10:33:52 +0300

    Asjust styles

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,7 +2,7 @@ import { Item } from "../../interfaces/api";
 
 export function Card(item: Item) {
   return (
-    <article className="py-4 px-5 rounded-lg shadow-sm">
+    <article className="py-6 px-5 shadow-sm">
       <a target="_blank" rel="noreferrer" href={item.url}>
         <h3 className="font-sans text-lg text-gray-800">{item.title}</h3>
       </a>
