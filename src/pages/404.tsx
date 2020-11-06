580176371ddb60a9e7fa3982b2cf5d2e632b7a93
Daniil Kutyavin <daniil.kutyavin94@gmail.com>
2020-11-06 21:19:06 +0300

    Added simple 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export function Page404() {
+  return (
+    <div>
+      Error 404. <Link to="/">Go home</Link>
+    </div>
+  )
+}
--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
+import { Page404 } from './404'
 import { TopPage } from './top'
 
 export function Root() {
   return (
     <Switch>
-      <Route path="/">
+      <Route exact path="/">
         <TopPage />
+      </Route>
+      <Route>
+        <Page404 />
       </Route>
     </Switch>
   )
