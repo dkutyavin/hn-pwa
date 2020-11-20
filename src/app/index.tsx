87e43de14114b645c5da1e2f4fbf668469327d0e
Daniil Kutyavin <daniil.kutyavin94@gmail.com>
2020-11-20 21:23:43 +0300

    setup tailwingcss

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { Layout } from '../components/layout'
 import { Root } from '../pages/root'
 import { Providers } from './providers'
+import './generated-styles.css'
 
 export function App() {
   return (
     <Providers>
-      <Root />
+      <Layout>
+        <Root />
+      </Layout>
     </Providers>
   )
 }
